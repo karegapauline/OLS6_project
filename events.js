f8bbe55a1d28e6ba1034265ada55db385b664307
karegapauline <karegapauline@gmail.com>
2023-01-18 15:27:48 +0300

    events page functionalities working

--- events.js
+++ events.js
@@ -1,23 +1,9 @@
-// Upon clicking this workshop button\
-var myArray = [
-    {'workshopName':'Introduction to Bacterial Genomics Bioinformatics Workshop', 'workshopDate': '23-27th January', 'applicationLink' : 'Sign up here: https://t.co/Bi1PN0bITb'},
-    {'workshopName':'Shotgun Metagenomics Analysis Workshop','workshopDate':'Dec 12-14','applicationLink':'Sign up here: https://t.co/kUx64lS6s4'}
-   ]
+function buildTable() {
+    // console.log("This button was clicked")
+    window.location.href='eventsjsontable.html';
+}    
 
-buildTable(myArray)
-
-    function buildTable(data){
-        var table = document.getElementById('myTable')
-
-            for (var i = 0; i < data.length; i++){
-                var row = `<tr>
-                                <td>${data[i].workshopName}</td>
-                                <td>${data[i].workshopDate}</td>
-                                <td>${data[i].applicationLink}</td>
-
-                            </tr>`
-                 table.innerHTML += row
-               }
-            }
-
-            
+function buildTable2() {
+    // console.log("This button was clicked")
+    window.location.href='conferencesjsontable.html';
+}

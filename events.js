6ae05877abcfb2522b1743859ca2bc2f58757ada
karegapauline <karegapauline@gmail.com>
2023-01-18 17:19:32 +0300

    resources page functionalities working

--- events.js
+++ events.js
@@ -7,3 +7,8 @@ function buildTable2() {
     // console.log("This button was clicked")
     window.location.href='conferencesjsontable.html';
 }
+
+function buildTableres() {
+    // console.log("This button was clicked")
+    window.location.href='post_highschool.html';
+}
